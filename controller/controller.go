5b4b72f123e9ec73562dd8e57090cdd49853a9ac
karokojnr <karoko.jnr99@gmail.com>
2020-06-15 22:29:21 +0300

    Work in progress

--- controller/controller.go
+++ controller/controller.go
@@ -117,7 +117,7 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	tokenString, err := token.SignedString([]byte("secret"))
 
 	// exp := time.Now().Add(time.Hour * time.Duration(1)).Unix()
-	exp := time.Now().Add(time.Hour * 15).Unix()
+	exp := time.Now().Add(time.Hour * 1).Unix()
 	fmt.Println(time.Now())
 	if err != nil {
 		res.Error = "Error while generating token,Try again"
