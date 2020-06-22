ec8c6e0b1601bf2e4d6891a1e94e4bb4d117d7e4
karokojnr <karoko.jnr99@gmail.com>
2020-06-22 22:46:26 +0300

    Work in progress

--- controller/controller.go
+++ controller/controller.go
@@ -497,7 +497,7 @@ func CallBackHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	paymentFilter := bson.M{"userId": id}
 	var payment model.Payment
-	update := bson.M{"$set": bson.M{"resultCode": payment.ResultCode}}
+	update := bson.M{"$set": bson.M{"resultCode": resultCode}}
 	_, errp := paymentCollection.UpdateOne(context.TODO(), paymentFilter, update)
 	if errp != nil {
 		fmt.Printf("error...")
