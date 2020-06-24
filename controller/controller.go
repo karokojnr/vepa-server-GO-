8306f06642d8c9a805930e2b29e63c6f1ff5257b
karokojnr <karoko.jnr99@gmail.com>
2020-06-24 03:10:16 +0300

    Work in progress

--- controller/controller.go
+++ controller/controller.go
@@ -503,7 +503,7 @@ func CallBackHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	resultCode := bd.(map[string]interface{})["Body"].(map[string]interface{})["stkCallback"].(map[string]interface{})["ResultCode"]
 	rBody := bd.(map[string]interface{})["Body"].(map[string]interface{})["stkCallback"].(map[string]interface{})["ResultDesc"]
-	mpesaReceiptNumber := bd.(map[string]interface{})["Body"].(map[string]interface{})["stkCallback"].(map[string]interface{})["CallbackMetadata"].(map[string]interface{})["Item"].(map[string]interface{})["MpesaReceiptNumber"]
+	mpesaReceiptNumber := bd.(map[string]interface{})["Body"].(map[string]interface{})["stkCallback"].(map[string]interface{})["CallbackMetadata"].(map[string]interface{})["Item"]["MpesaReceiptNumber"]
 
 	log.Println("Mpesa Receipt Number:")
 	log.Println(mpesaReceiptNumber)
