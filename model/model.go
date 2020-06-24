cf3a555f5407b16ec5670fcb4e1c65b6011edd70
karokojnr <karoko.jnr99@gmail.com>
2020-06-25 00:09:43 +0300

    Work in progress

--- model/model.go
+++ model/model.go
@@ -2,7 +2,7 @@ package model
 
 import (
 	// "go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson"
+	// "go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
