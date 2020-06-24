6a6b54fe414367bd94d5ea00c13784a829a0a8aa
karokojnr <karoko.jnr99@gmail.com>
2020-06-25 00:27:31 +0300

    Work in progress

--- controller/controller.go
+++ controller/controller.go
@@ -576,6 +576,10 @@ func UserPaymentsHandler(w http.ResponseWriter, r *http.Request) {
 	})
 	var res model.ResponseResult
 	var results []*model.Payment
+	errr := json.Unmarshal([]byte(s), &results)
+	if errr != nil {
+		log.Println("Unmarshall error")
+	}
 	collection, err := db.GetPaymentCollection()
 	if err != nil {
 		res.Error = "Error, Try Again Later"
