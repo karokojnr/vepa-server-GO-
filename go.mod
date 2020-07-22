4bf297ee9e58f3a304bd4bde513a8867166ba7f4
karokojnr <karoko.jnr99@gmail.com>
2020-07-22 03:33:30 +0300

    :ENV: :shit:

--- go.mod
+++ go.mod
@@ -4,11 +4,12 @@ go 1.14
 
 require (
 	github.com/AndroidStudyOpenSource/mpesa-api-go v0.0.0-20200405214254-b756218379d3
-	github.com/Iwark/pushnotification v0.0.0-20160811085603-d38f6108f22f
-	github.com/aws/aws-sdk-go v1.33.8
+	github.com/appleboy/go-fcm v0.1.5
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gorilla/mux v1.7.4
 	github.com/joho/godotenv v1.3.0
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/stretchr/testify v1.5.1 // indirect
 	go.mongodb.org/mongo-driver v1.3.4
 	golang.org/x/crypto v0.0.0-20200604202706-70a84ac30bf9
 )
--- util/notification.go
+++ util/notification.go
@@ -1,56 +1,33 @@
 package util
 
 import (
+	"fmt"
 	"log"
-	"os"
-
-	"github.com/Iwark/pushnotification"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/joho/godotenv"
+	"github.com/appleboy/go-fcm"
 )
 
 func SendNotifications(fcmToken string, notificationBody string) {
-	////Send message...
-	//msg := &fcm.Message{
-	//	To: fcmToken,
-	//	Data: map[string]interface{}{
-	//		"title": "Vepa",
-	//		"body":  &notificationBody,
-	//	},
-	//}
-	//// Create a FCM client to send the message.
-	//// env.GoDotEnvVariable("FCM_SERVER_KEY")
-	//client, err := fcm.NewClient(GoDotEnvVariable("FCM_SERVER_KEY"))
-	//if err != nil {
-	//	log.Fatalln(err)
-	//}
-	//// Send the message and receive the response without retries.
-	//response, err := client.Send(msg)
-	//if err != nil {
-	//	log.Fatalln(err)
-	//}
-	//log.Printf("%#v\n", response)
-	//fmt.Println("notification sent...")
-	//return
-
-	if err := godotenv.Load(); err != nil {
-		log.Fatal(err)
+	//Send message...
+	msg := &fcm.Message{
+		To: fcmToken,
+		Data: map[string]interface{}{
+			"title": "Vepa",
+			"body":  &notificationBody,
+		},
 	}
-
-	push := pushnotification.Service{
-		AWSAccessKey:         os.Getenv("AWSAccessKey"),
-		AWSAccessSecret:      os.Getenv("AWSAccessSecret"),
-		AWSSNSApplicationARN: os.Getenv("AWSSNSApplicationARN"),
-		AWSRegion:            os.Getenv("AWSRegion"),
-	}
-
-	err := push.Send(os.Getenv("DeviceToken"), &pushnotification.Data{
-		Alert: aws.String(notificationBody),
-		Sound: aws.String("default"),
-		Badge: aws.Int(1),
-	})
+	// Create a FCM client to send the message.
+	// env.GoDotEnvVariable("FCM_SERVER_KEY")
+	client, err := fcm.NewClient(GoDotEnvVariable("FCM_SERVER_KEY"))
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
 	}
+	// Send the message and receive the response without retries.
+	response, err := client.Send(msg)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Printf("%#v\n", response)
+	fmt.Println("notification sent...")
+	return
 
 }
